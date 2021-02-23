7ee2275631c8f477b653907dc0050ef0700ec48f
LinowskiD <d.linowski@gmail.com>
2021-02-23 20:34:09 +0100

    Solved issue with sigal spy

--- hdl_tb/counter_tb_lib.vhd
+++ hdl_tb/counter_tb_lib.vhd
@@ -25,22 +25,18 @@ architecture tb of counter_tb is
   signal counter_en, counter_rst : std_logic;
   signal counter_pre : std_logic_vector(c_preload_bit_size - 1 downto 0);
 
-  alias spy_dut_counter is << signal .counter_tb.uut.counter : unsigned >>;
-
 begin
   main : process
+  alias spy_dut_counter is << signal .counter_tb.uut.counter : unsigned >>;
   begin
     test_runner_setup(runner, runner_cfg);
     while test_suite loop
       if run("test_0001_values_in_reset") then
         info("* REQ_SEG_0020");
-        info("Testing in reset");
         check_equal(dut_rst_n, '0', "Reset shall be active");
         wait until rising_edge(dut_clk);
-        report to_string(spy_dut_counter);
-        check_equal(dut_done, '0', "Default value shall be used");
-        -- TODO: solve spying internal counter
-        -- check_equal(spy_dut_counter, to_unsigned(0, c_preload_bit_size), "Default value shall be used");
+        check_equal(dut_done, '0', "done signal shall be 0 in reset");
+        check_equal(spy_dut_counter, to_unsigned(0, c_preload_bit_size), "Internal counter shall be all zeroes");
       end if;
     end loop;
     test_runner_cleanup(runner); -- Simulation ends here
