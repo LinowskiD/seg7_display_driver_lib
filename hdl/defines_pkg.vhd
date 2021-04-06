e63775b03f6d57f9ad5f3083c75d558ca28d7698
LinowskiD <d.linowski@gmail.com>
2021-04-06 16:58:10 +0200

    clean up in types and definitions

--- hdl/defines_pkg.vhd
+++ hdl/defines_pkg.vhd
@@ -2,8 +2,38 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+library seg7_display_driver_lib;
+use seg7_display_driver_lib.types_pkg.all;
+
 package defines_pkg is
 
   constant c_number_of_digits_default : natural := 4;
+  constant c_seg_pos : t_segment_pos := (
+    ca => 6,
+    cb => 5,
+    cc => 4,
+    cd => 3,
+    ce => 2,
+    cf => 1,
+    cg => 0
+  );
+  constant c_digit_map_to_seg : t_digit_map_to_seg := (
+    16#0# => "0000001",
+    16#1# => "1001111",
+    16#2# => "0010010",
+    16#3# => "0000110",
+    16#4# => "1001100",
+    16#5# => "0100100",
+    16#6# => "0100000",
+    16#7# => "0001111",
+    16#8# => "0000000",
+    16#9# => "0000100",
+    16#A# => "0000010",
+    16#B# => "1100000",
+    16#C# => "0110001",
+    16#D# => "1000010",
+    16#E# => "0110000",
+    16#F# => "0111000"
+  );
 
 end package defines_pkg;
--- hdl/types_pkg.vhd
+++ hdl/types_pkg.vhd
@@ -2,9 +2,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-library seg7_display_driver_lib;
-use seg7_display_driver_lib.defines_pkg.all;
-
 package types_pkg is
 
   subtype t_digit is std_logic_vector(3 downto 0);
@@ -19,5 +16,16 @@ package types_pkg is
     cf : std_logic;
     cg : std_logic;
   end record;
+  type t_segment_pos is record
+    ca : natural;
+    cb : natural;
+    cc : natural;
+    cd : natural;
+    ce : natural;
+    cf : natural;
+    cg : natural;
+  end record;
+  type t_digit_map_to_seg is array (16#0# to 16#F#) of std_logic_vector(6 downto 0);
+
 
 end package types_pkg;
