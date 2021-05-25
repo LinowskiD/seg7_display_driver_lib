9c3770461a3872e99c3e252c929753a2e91dc392
LinowskiD <d.linowski@gmail.com>
2021-05-25 23:01:47 +0200

    Updated run.py to be more robust

--- script/run.py
+++ script/run.py
@@ -1,19 +1,33 @@
 from vunit import VUnit
 from pathlib import Path
 import sys
+import os
 
-module_name = "seg7_display_driver"
-src_path = Path(__file__).parent/ "../"
-
+# ROOT
+ROOT = Path(__file__).resolve().parents[1]
+# WORK
+WORK = "work"
+# Sources path for DUT
+DUT_PATH = ROOT / "hdl"
+# Sources path for TB
+TEST_PATH = ROOT / "hdl_tb"
+# MODULE_NAME
+MODULE_NAME = os.path.basename(ROOT).split("_lib")[0]
+# Append arguments to VUnit call
 sys.argv.append("-o")
-sys.argv.append("{}/work/".format(src_path))
-
+sys.argv.append(f"{ROOT / WORK}")
 # Create VUnit instance by parsing command line arguments
 vu = VUnit.from_argv()
+vu.enable_location_preprocessing()
+# Add OSVVM
 # vu.add_osvvm()
-
-vu.add_library(f"{module_name}_lib").add_source_files(src_path / "hdl" / "*.vhd", vhdl_standard="2008")
-vu.add_library(f"{module_name}_tb_lib").add_source_files(src_path / "hdl_tb" / "*.vhd", vhdl_standard="2008")
-
+# Create design library
+module_lib = vu.add_library(f"{MODULE_NAME}_lib")
+# Add design source files to module_lib
+module_lib.add_source_files([DUT_PATH / "*.vhd"], vhdl_standard="2008")
+# Create testbench library
+tb_lib = vu.add_library(f"{MODULE_NAME}_tb_lib")
+# Add testbench source files to tb_lib
+tb_lib.add_source_files([TEST_PATH / "*.vhd"], vhdl_standard="2008")
 # Run vunit function
 vu.main()
