92753c712eb0ba18b2ed524f80f9c817ba8bd278
Dawid Linowski <d.linowski@gmail.com>
2022-12-04 22:33:49 +0100

    Test 0002 fixed

--- hdl_tb/driver_top_tb.vhd
+++ hdl_tb/driver_top_tb.vhd
@@ -12,6 +12,12 @@ use seg7_display_driver_lib.driver_pkg.all;
 library seg7_display_driver_tb_lib;
 use seg7_display_driver_tb_lib.driver_tb_pkg.all;
 
+-- TESTS:
+-- * signals in reset
+-- * digit change
+-- * segment update
+-- * digit ON time update
+
 entity driver_top_tb is
   generic (runner_cfg : string := runner_cfg_default);
 end entity;
@@ -20,8 +26,8 @@ architecture tb of driver_top_tb is
 
   signal dut_clk                  : std_logic := '0';
   signal dut_rst_n                : std_logic := '0';
-  signal dut_digit_on_time        : std_logic_vector((c_digit_change_interval_bit_size - 1) downto 0);
-  signal dut_value                : t_value((c_number_of_digits - 1) downto 0);
+  signal dut_digit_on_time        : std_logic_vector((c_digit_change_interval_bit_size - 1) downto 0) := std_logic_vector(to_unsigned(c_digit_change_interval, c_digit_change_interval_bit_size));
+  signal dut_value                : t_value((c_number_of_digits - 1) downto 0) := (others => X"0");
   signal dut_digit_select         : std_logic_vector((c_number_of_digits - 1) downto 0);
   signal dut_digit_select_stable  : t_boolean_array((c_number_of_digits - 1) downto 0);
   signal dut_segments             : t_segments;
@@ -98,52 +104,52 @@ begin
           check_equal(dut_digit_select(digit_nmb), '0', result("for digit_select when in reset"));
         end loop;
         info("===== TEST CASE FINISHED =====");
-      -- elsif run("test_0002_digit_change") then
-      --   info(separator);
-      --   info("===== TEST CASE STARTED =====");
-      --   info("TEST CASE: test_0002_digit_change");
-      --   info(separator);
-      --   info("Release reset and provide input");
-      --   dut_rst_n <= '1';
-      --   for digit_nmb in 0 to c_number_of_digits - 1 loop
-      --     dut_digits(get_slice_range(digit_nmb)'range) <= X"F";
-      --   end loop;
-      --   walk(dut_clk, 1);
-      --   info("* dut_digit_select");
-      --   for digit_nmb in 0 to c_number_of_digits - 1 loop
-      --     if digit_nmb = 0 then
-      --       check_equal(dut_digit_select(digit_nmb), '1', result("for digit_select(" & integer'image(digit_nmb) & ") after reset release"));
-      --     else
-      --       check_equal(dut_digit_select(digit_nmb), '0', result("for digit_select(" & integer'image(digit_nmb) & ") after reset release"));
-      --     end if;
-      --   end loop;
-      --   info("Wait for 1st digit change - using default ON/OFF ratio (100% ON)");
-      --   wait until dut_digit_select(1) = '1' for c_digit_change_interval_time;
-      --   for digit_nmb in 0 to c_number_of_digits - 1 loop
-      --     if digit_nmb = 1 then
-      --       check_equal(dut_digit_select(digit_nmb), '1', result("for digit_select(" & integer'image(digit_nmb) & ") after reset release"));
-      --     else
-      --       check_equal(dut_digit_select(digit_nmb), '0', result("for digit_select(" & integer'image(digit_nmb) & ") after reset release"));
-      --     end if;
-      --   end loop;
-      --   info("Wait for next changes and verify pin state");
-      --   v_time_start := now;
-      --   for step_nmb in 2 to 10 loop
-      --     info("Verifying change no. " & integer'image((step_nmb)));
-      --     wait for 1 ps;
-      --     wait until dut_digit_select(step_nmb mod c_number_of_digits) = '1' for c_digit_change_interval_time;
-      --     check_equal(now - v_time_start, c_digit_change_interval_time, result("for digit_select(" & integer'image(step_nmb mod c_number_of_digits) & ")"));
-      --     v_time_start := now;
-      --     for digit_nmb in 0 to c_number_of_digits - 1 loop
-      --       if digit_nmb = (step_nmb mod c_number_of_digits) then
-      --         check_equal(dut_digit_select(digit_nmb), '1', result("for digit_select(" & integer'image(digit_nmb) & ") when in operation"));
-      --       else
-      --         check_equal(dut_digit_select(digit_nmb), '0', result("for digit_select(" & integer'image(digit_nmb) & ") when in operation"));
-      --       end if;
-      --       check_equal(dut_digit_select_stable(digit_nmb), true, result("for stability stability check on digit_select(" & integer'image(digit_nmb)) & ")");
-      --     end loop;
-      --   end loop;
-      --   info("===== TEST CASE FINISHED =====");
+      elsif run("test_0002_digit_change") then
+        info(separator);
+        info("===== TEST CASE STARTED =====");
+        info("TEST CASE: test_0002_digit_change");
+        info(separator);
+        info("Release reset and provide input");
+        dut_rst_n <= '1';
+        for digit_nmb in 0 to c_number_of_digits - 1 loop
+          dut_value(digit_nmb) <= X"F";
+        end loop;
+        walk(dut_clk, 1);
+        info("* dut_digit_select");
+        for digit_nmb in 0 to c_number_of_digits - 1 loop
+          if digit_nmb = 0 then
+            check_equal(dut_digit_select(digit_nmb), '1', result("for digit_select(" & integer'image(digit_nmb) & ") after reset release"));
+          else
+            check_equal(dut_digit_select(digit_nmb), '0', result("for digit_select(" & integer'image(digit_nmb) & ") after reset release"));
+          end if;
+        end loop;
+        info("Wait for 1st digit change - using default ON/OFF ratio (100% ON)");
+        wait until dut_digit_select(1) = '1' for c_digit_change_interval_time;
+        for digit_nmb in 0 to c_number_of_digits - 1 loop
+          if digit_nmb = 1 then
+            check_equal(dut_digit_select(digit_nmb), '1', result("for digit_select(" & integer'image(digit_nmb) & ") after reset release"));
+          else
+            check_equal(dut_digit_select(digit_nmb), '0', result("for digit_select(" & integer'image(digit_nmb) & ") after reset release"));
+          end if;
+        end loop;
+        info("Wait for next changes and verify pin state");
+        v_time_start := now;
+        for step_nmb in 2 to 10 loop
+          info("Verifying change no. " & integer'image((step_nmb)));
+          wait for 1 ps;
+          wait until dut_digit_select(step_nmb mod c_number_of_digits) = '1' for c_digit_change_interval_time;
+          check_equal(now - v_time_start, c_digit_change_interval_time, result("for digit_select(" & integer'image(step_nmb mod c_number_of_digits) & ")"));
+          v_time_start := now;
+          for digit_nmb in 0 to c_number_of_digits - 1 loop
+            if digit_nmb = (step_nmb mod c_number_of_digits) then
+              check_equal(dut_digit_select(digit_nmb), '1', result("for digit_select(" & integer'image(digit_nmb) & ") when in operation"));
+            else
+              check_equal(dut_digit_select(digit_nmb), '0', result("for digit_select(" & integer'image(digit_nmb) & ") when in operation"));
+            end if;
+            check_equal(dut_digit_select_stable(digit_nmb), true, result("for stability stability check on digit_select(" & integer'image(digit_nmb)) & ")");
+          end loop;
+        end loop;
+        info("===== TEST CASE FINISHED =====");
       -- elsif run("test_0003_segment_change") then
       --   info(separator);
       --   info("TEST CASE: test_0003_segment_change");
